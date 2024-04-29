3fdee1aef77e21ef49109ba850b6834d3fef1e3a
Xavier Lora <104650618+XavierLora@users.noreply.github.com>
2024-04-29 12:36:00 -0400

    Updates to invalid instructor parameters passed

--- dbApp/dbApp/views.py
+++ dbApp/dbApp/views.py
@@ -69,37 +69,44 @@ def admin_dashboard_view(request):
                 template = loader.get_template('dbApp/admin_dashboard.html')
                 return HttpResponse(template.render(context, request))
             elif action == 'performance':
-                instructor_name = request.GET.get('instructor')
-                instructor_semester = request.GET.get('semester')
-                instructor_year = request.GET.get('year')
-                print(instructor_name, instructor_semester, instructor_year)
-                instructor = Instructor.objects.get(name=instructor_name)
-                instructor_id = instructor.id
-                instructor_performance = []
-                # Query to get courses taught by the instructor in a specific semester and year
-                courses_taught = Teaches.objects.values('teacher_id', 'course_id', 'sec_id', 'semester', 'year')
-                courses_taught_filter = courses_taught.filter(teacher_id=instructor_id, semester=instructor_semester, year=instructor_year)
-                courses_taught_filter_count = courses_taught.filter(teacher_id=instructor_id, semester=instructor_semester, year=instructor_year).count()
-                enrollment_count = []
-                for course in courses_taught_filter:
-                    enrollment_count = Takes.objects.filter(course_id=course['course_id'], sec_id=course['sec_id'], semester=course['semester'], year=course['year']).count()
+                try:
+                    instructor_name = request.GET.get('instructor')
+                    instructor_semester = request.GET.get('semester')
+                    instructor_year = request.GET.get('year')
+                    print(instructor_name, instructor_semester, instructor_year)
+                    instructor = Instructor.objects.get(name=instructor_name)
+                    instructor_id = instructor.id
+                    instructor_performance = []
+                    # Query to get courses taught by the instructor in a specific semester and year
+                    courses_taught = Teaches.objects.values('teacher_id', 'course_id', 'sec_id', 'semester', 'year')
+                    courses_taught_filter = courses_taught.filter(teacher_id=instructor_id, semester=instructor_semester, year=instructor_year)
+                    courses_taught_filter_count = courses_taught.filter(teacher_id=instructor_id, semester=instructor_semester, year=instructor_year).count()
+                    enrollment_count = []
+                    for course in courses_taught_filter:
+                        enrollment_count = Takes.objects.filter(course_id=course['course_id'], sec_id=course['sec_id'], semester=course['semester'], year=course['year']).count()
 
-                instructor_funding = Funding.objects.values('name', 'semester', 'funding', 'year')
-                instructor_funding_filter = instructor_funding.filter(name=instructor_name, year=instructor_year, semester = instructor_semester)
-                instructor_funding_sum = instructor_funding_filter.aggregate(total_funding=Sum('funding'))
-                total_funding_sum = instructor_funding_sum['total_funding'] if instructor_funding_sum['total_funding'] else 0
+                    instructor_funding = Funding.objects.values('name', 'semester', 'funding', 'year')
+                    instructor_funding_filter = instructor_funding.filter(name=instructor_name, year=instructor_year, semester = instructor_semester)
+                    instructor_funding_sum = instructor_funding_filter.aggregate(total_funding=Sum('funding'))
+                    total_funding_sum = instructor_funding_sum['total_funding'] if instructor_funding_sum['total_funding'] else 0
 
-                instructor_papers = Papers.objects.values('name', 'semester', 'papers', 'years')
-                print(instructor_papers)
-                instructor_papers_filter = instructor_papers.filter(name=instructor_name, years=instructor_year, semester = instructor_semester)
-                print(instructor_papers_filter)
-                instructor_papers_sum = instructor_papers_filter.aggregate(total_papers=Sum('papers'))
-                papers_count = instructor_papers_sum.get('total_papers', 0)
-                instructor_performance.append((instructor.name, courses_taught_filter_count, enrollment_count, total_funding_sum, papers_count))
-                print(instructor_performance)
-                context = {'instructor_performance': instructor_performance}
-                template = loader.get_template('dbApp/admin_dashboard.html')
-                return HttpResponse(template.render(context, request))
+                    instructor_papers = Papers.objects.values('name', 'semester', 'papers', 'years')
+                    print(instructor_papers)
+                    instructor_papers_filter = instructor_papers.filter(name=instructor_name, years=instructor_year, semester = instructor_semester)
+                    print(instructor_papers_filter)
+                    instructor_papers_sum = instructor_papers_filter.aggregate(total_papers=Sum('papers'))
+                    papers_count = instructor_papers_sum.get('total_papers', 0)
+                    instructor_performance.append((instructor.name, courses_taught_filter_count, enrollment_count, total_funding_sum, papers_count))
+                    print(instructor_performance)
+                    context = {'instructor_performance': instructor_performance}
+                    template = loader.get_template('dbApp/admin_dashboard.html')
+                    return HttpResponse(template.render(context, request))
+                except Instructor.DoesNotExist:
+                    print("Invalid Instructor Input attempt")
+                    messages.error(request, 'Invalid instructor passed.')
+                    template = loader.get_template('dbApp/admin_dashboard.html')
+                    context = {'error': True}
+                    return HttpResponse(template.render(context, request))
     else:
         template = loader.get_template('dbApp/admin_dashboard.html')
         context = {}
@@ -110,50 +117,62 @@ def instructor_dashboard_view(request):
         if 'action' in request.GET:
             action = request.GET.get('action')
             if action == 'course_summary':
-                instructor_name = request.GET.get('instructor')
-                semester = request.GET.get('semester')
-                year = request.GET.get('year')
+                try:
+                    instructor_name = request.GET.get('instructor')
+                    semester = request.GET.get('semester')
+                    year = request.GET.get('year')
 
-                # Query to get instructor ID based on name
-                instructor = Instructor.objects.get(name=instructor_name)
-                instructor_id = instructor.id
-                # Query to get courses taught by the instructor in a specific semester and year
-                courses_taught = Teaches.objects.values('teacher_id', 'course_id', 'sec_id', 'semester', 'year')
-                courses_taught_filter = courses_taught.filter(teacher_id=instructor_id, semester=semester, year=year)
+                    # Query to get instructor ID based on name
+                    instructor = Instructor.objects.get(name=instructor_name)
+                    instructor_id = instructor.id
+                    # Query to get courses taught by the instructor in a specific semester and year
+                    courses_taught = Teaches.objects.values('teacher_id', 'course_id', 'sec_id', 'semester', 'year')
+                    courses_taught_filter = courses_taught.filter(teacher_id=instructor_id, semester=semester, year=year)
 
-                print(courses_taught_filter)
+                    print(courses_taught_filter)
 
-                # Query to get student enrollments for each course
-                course_enrollments = []
-                for course in courses_taught_filter:
-                    enrollment_count = Takes.objects.filter(course_id=course['course_id'], sec_id=course['sec_id'], semester=course['semester'], year=course['year']).count()
-                    course_enrollments.append((f"{course['course_id']} - {course['sec_id']}", enrollment_count))
-                
-                context = {'course_enrollments': course_enrollments}
-                template = loader.get_template('dbApp/instructor_dashboard.html')
-                return HttpResponse(template.render(context, request))
+                    # Query to get student enrollments for each course
+                    course_enrollments = []
+                    for course in courses_taught_filter:
+                        enrollment_count = Takes.objects.filter(course_id=course['course_id'], sec_id=course['sec_id'], semester=course['semester'], year=course['year']).count()
+                        course_enrollments.append((f"{course['course_id']} - {course['sec_id']}", enrollment_count))
+                    
+                    context = {'course_enrollments': course_enrollments}
+                    template = loader.get_template('dbApp/instructor_dashboard.html')
+                    return HttpResponse(template.render(context, request))
+                except Instructor.DoesNotExist:
+                    print("Invalid Instructor Input attempt")
+                    messages.error(request, 'Invalid instructor passed.')
+                    template = loader.get_template('dbApp/instructor_dashboard.html')
+                    context = {'error': True}
+                    return HttpResponse(template.render(context, request))
             elif action == 'student_enrollment':
-                instructor_name = request.GET.get('instructor')
-                course_semester = request.GET.get('semester')
-                course_section = request.GET.get('section')
-                course = request.GET.get('course')
-                # Query to get instructor ID based on name
-                instructor = Instructor.objects.get(name=instructor_name)
-                instructor_id = instructor.id
-                # Query to get courses taught by the instructor in a specific semester and year
-                students_taught = Takes.objects.values('student_id', 'course_id', 'sec_id', 'semester', 'year')
-                students_taught_filter = students_taught.filter(course_id = course, sec_id = course_section, semester = course_semester)
-                student_ids = [student['student_id'] for student in students_taught_filter]
-                students_names = Students.objects.values('name', 'student_id')
-                students_names_filter = students_names.filter(student_id__in=student_ids)
-                print(students_names_filter)
-                # You can further process students_names_filter if needed
-    
-                context = {'students_names_filter': students_names_filter}
-                template = loader.get_template('dbApp/instructor_dashboard.html')
-                return HttpResponse(template.render(context, request))
-
-    
+                try:
+                    instructor_name = request.GET.get('instructor')
+                    course_semester = request.GET.get('semester')
+                    course_section = request.GET.get('section')
+                    course = request.GET.get('course')
+                    # Query to get instructor ID based on name
+                    instructor = Instructor.objects.get(name=instructor_name)
+                    instructor_id = instructor.id
+                    # Query to get courses taught by the instructor in a specific semester and year
+                    students_taught = Takes.objects.values('student_id', 'course_id', 'sec_id', 'semester', 'year')
+                    students_taught_filter = students_taught.filter(course_id = course, sec_id = course_section, semester = course_semester)
+                    student_ids = [student['student_id'] for student in students_taught_filter]
+                    students_names = Students.objects.values('name', 'student_id')
+                    students_names_filter = students_names.filter(student_id__in=student_ids)
+                    print(students_names_filter)
+                    # You can further process students_names_filter if needed
+        
+                    context = {'students_names_filter': students_names_filter}
+                    template = loader.get_template('dbApp/instructor_dashboard.html')
+                    return HttpResponse(template.render(context, request))
+                except (Instructor.DoesNotExist, Takes.DoesNotExist):
+                    print("Invalid Instructor Input attempt")
+                    messages.error(request, 'Invalid instructor passed.')
+                    template = loader.get_template('dbApp/instructor_dashboard.html')
+                    context = {'error': True}
+                    return HttpResponse(template.render(context, request))
     else:
         template = loader.get_template('dbApp/instructor_dashboard.html')
         context = {}
